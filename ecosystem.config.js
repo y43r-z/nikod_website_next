35fad88425dc5683f86dcdf1075c2f20ddecfaf0
y43r-z <yaserzarvandi1@gmail.com>
2025-11-12 01:14:41 +0330

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [{
+      name: 'next-app',
+      script: 'npm',
+      args: 'start',
+      cwd: 'D:/yaser/nikod_website_next',
+      env: {
+        PORT: 4000,
+        HOST: '0.0.0.0'
+      }
+    }]
+  }
